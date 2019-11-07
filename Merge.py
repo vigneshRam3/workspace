cb38396b880cb5aff16966816fcc4b77810515d7
vigneshRam3 <40824992+vigneshRam3@users.noreply.github.com>
2019-11-07 16:58:22 +0530

    Update Merge.py

--- Merge.py
+++ Merge.py
@@ -1,47 +1,41 @@
-import json, os, sys
+import json, sys, os
 
-filepath = input('Folder Path: ')
-prefix = input('Input File Base Name: ')
-final_name = input('Output File Base Name: ')
-max_size = int(input('Max File Size: '))
-final = {}
-
-files = [i for i in os.listdir(filepath) if os.path.isfile(os.path.join(filepath,i)) and \
-         prefix in i]
+Path = input('Enter the folder path: ')
+BaseName = input('Enter the Input File Base Name Of The File: ')
+RetrievalName = input('Enter the Output File Base Name Of The File: ')
+maxSize = int(input('Enter the Maximum File Size: '))
+files = [i for i in os.listdir(Path) if os.path.isfile(os.path.join(Path,i)) and \
+         BaseName in i]
+Final = {}
 for i in files:
-    with open(filepath + i, 'r') as json_file:
+    with open(Path + i, 'r') as json_file:
         obj = dict(json.load(json_file))
-        if not final.items():
-            final = obj.copy()
+        if not Final.items():
+            Final = obj.copy()
         else:
-            final[list(final.keys())[0]].extend(obj[list(obj.keys())[0]])
-            # for finding the rootkey of the list of objects dynamically
-rootkey = str(list(final.keys())[0])
-
-#writes the obtained objects in a single file
-# json.dump(final, open('all_objects_merged.json', 'w+'))
-
-#to write the objects into seperate files with max_size
-file_iter = 1
-list_size = len(final[rootkey])
+            Final[list(Final.keys())[0]].extend(obj[list(obj.keys())[0]])
+           
+rootkey = str(list(Final.keys())[0])
+itr = 1
+lstSize = len(Final[rootkey])
 template = "{\"" + str(rootkey) + "\": []" + "}"
 
-with open(final_name + str(file_iter) + '.json', 'w+') as fp:
+with open(RetrievalName + str(itr) + '.json', 'w+') as fp:
     fp.write(template)
 
-for i in range(list_size):
+for i in range(lstSize):
     temp = ''
-    fsize = os.stat(final_name + str(file_iter) + '.json').st_size #current file's size
-    isize = len(str(final[rootkey][i]).encode('utf-8')) #size of the text to be written (current list item)
-    if not (fsize + isize) < max_size:
-        file_iter += 1
-        with open(final_name + str(file_iter) + '.json', 'w+') as fp:
-            fp.write(template)
+    fsize = os.stat(RetrievalName + str(itr) + '.json').st_size 
+    isize = len(str(Final[rootkey][i]).encode('utf-8')) 
+    if not (fsize + isize) < maxSize:
+        itr += 1
+        with open(RetrievalName + str(itr) + '.json', 'w+') as fp:
+            fptr.write(template)
 
-    with open(final_name + str(file_iter) + '.json', 'r') as fp:
+    with open(RetrievalName + str(itr) + '.json', 'r') as fp:
         temp = fp.read()
 
-    with open(final_name + str(file_iter) + '.json', 'w+') as fp:
+    with open(RetrievalName + str(itr) + '.json', 'w+') as fp:
         temp = json.loads(temp)
-        temp[rootkey].append(final[rootkey][i])
+        temp[rootkey].append(Final[rootkey][i])
         json.dump(temp, fp)
